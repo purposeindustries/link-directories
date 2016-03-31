8d8e9ebf37f53a39628c294f2f8662e93cba2a16
Bence Dányi <bence@danyi.me>
2016-03-31 13:34:21 +0200

    fix: scream on error

--- index.js
+++ index.js
@@ -11,20 +11,20 @@ try {
   fs.statSync(packageJSON);
 } catch(x) {
   debug('package.json does not exist here: %s', packageJSON);
-  process.exit(0);
+  process.exit(1);
 }
 
 try{
   json = require(packageJSON);
 } catch(x) {
   debug('failed to parse package.json from here: %s', packageJSON);
-  process.exit(0);
+  process.exit(1);
 }
 
 links = json['link-directories'];
 if (!Array.isArray(links)) {
   debug('links does not seem to an array: %j', links);
-  process.exit(0);
+  process.exit(1);
 }
 
 try {
